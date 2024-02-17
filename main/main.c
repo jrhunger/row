bb84e696bd5f51d54233d77b460a342b2e250a58
jrhunger <1101010@gmail.com>
2024-02-17 14:29:55 -0500

    working w/LCD

--- main/main.c
+++ main/main.c
@@ -15,6 +15,8 @@
 #include "freertos/queue.h"
 #include "driver/gpio.h"
 #include "esp_timer.h"
+#include "esp_adc/adc_oneshot.h"
+#include "hd44780.h"
 
 /**
  * Brief: Replacement controller for NordicTrack RW600 Rower
@@ -27,22 +29,28 @@
  * - Drive the magnetic-resistance-adjust motor to adjust resistance
  *
  * GPIO status:
- * GPIO5:  input, pulled up, interrupt from rising edge.
- * GPIO19: output (ESP32C2/ESP32H2 uses GPIO9 as the second output pin)
- * - can be connected to GPIO5 to simulate the reed switch
- * GPIO2 / ADC1 channel 2: potentiometer
- *
+ * // Reed Sensor (can connect to GPIO21/U0TXD for test input)
+ * GPIO8: input  - rotor reed sensor - pulled up, interrupt from rising edge.
+ * 
+ * // Motor control
+ * GPIO2 / ADC1 channel 2: control potentiometer
+ * GPIO3 / ADC1 channel 3: motor feedback potentiometer
+ * GPIO0: output - motor forward
+ * GPIO1: output - motor reverse
+ * 
+ * // LCD HD4478
+ * GPIO18: output - LCD EN
+ * GPIO19: output - LCD RS
+ * GPIO04: output - LCD D4
+ * GPIO05: output - LCD D5
+ * GPIO06: output - LCD D6
+ * GPIO07: output - LCD D7
  */
 
-/*
- * Let's say, GPIO_INPUT_IO_0=5, in binary representation,
- * 1ULL<<GPIO_INPUT_IO_1 is equal to 0000000000000000000000000000000000100000
- * */
-#define GPIO_INPUT_IO_0     CONFIG_GPIO_INPUT_0
+//*** Manual GPIO Config
+//#define GPIO_INPUT_IO_0     CONFIG_GPIO_REED_INPUT
+#define GPIO_INPUT_IO_0  GPIO_NUM_8
 #define GPIO_INPUT_PIN_SEL  (1ULL<<GPIO_INPUT_IO_0)
-
-#define GPIO_OUTPUT_IO_0    CONFIG_GPIO_OUTPUT_0
-#define GPIO_OUTPUT_PIN_SEL  (1ULL<<GPIO_OUTPUT_IO_0)
 
 #define ESP_INTR_FLAG_DEFAULT 0
 
@@ -61,33 +69,24 @@ static void gpio_task(void* arg)
     uint32_t io_num;
     for (;;) {
         if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
-            if (io_num == 5) {
+            if (io_num == GPIO_INPUT_IO_0) {
                 trigger_count++;
+            }
+            else {
+                printf("isr for io %" PRIu32 "\n", io_num);
             }
         }
     }
 }
 
+static int adc_raw[2][10];
 void app_main(void)
 {
-    //zero-initialize the config structure.
-    gpio_config_t io_conf = {};
-    
-    // configure test output pin
-    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pull_down_en = 0;
-    io_conf.pull_up_en = 0;
-    gpio_config(&io_conf);
-
-    
-    // configure reed input pin
-    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-    io_conf.intr_type = GPIO_INTR_POSEDGE; // interrupt on rising edge
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pull_up_en = 1;
-    gpio_config(&io_conf);
+    // initialize the reed input switch GPIO
+    gpio_pullup_en(GPIO_INPUT_IO_0);
+    gpio_set_direction(GPIO_INPUT_IO_0, GPIO_MODE_INPUT);
+    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_POSEDGE);
+    gpio_intr_enable(GPIO_INPUT_IO_0);
 
     //create a queue to handle gpio event from isr
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
@@ -99,19 +98,62 @@ void app_main(void)
     //hook isr handler for specific gpio pin
     gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
 
+    // ADC1 Init
+    adc_oneshot_unit_handle_t adc1_handle;
+    adc_oneshot_unit_init_cfg_t init_config1 = {
+        .unit_id = ADC_UNIT_1,
+        .ulp_mode = ADC_ULP_MODE_DISABLE,
+        .clk_src = ADC_DIGI_CLK_SRC_DEFAULT
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+
+    // ADC1 Channel Config
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+        .atten = ADC_ATTEN_DB_11
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_2, &config));
+
+    hd44780_t lcd = {
+        .write_cb = NULL,
+        .font = HD44780_FONT_5X8,
+        .lines = 2,
+        .pins = {
+            .e  = GPIO_NUM_18,
+            .rs = GPIO_NUM_19,
+            .d4 = GPIO_NUM_4,
+            .d5 = GPIO_NUM_5,
+            .d6 = GPIO_NUM_6,
+            .d7 = GPIO_NUM_7,
+            .bl = HD44780_NOT_USED
+        }
+    };
+    ESP_ERROR_CHECK(hd44780_init(&lcd));
+    hd44780_gotoxy(&lcd,0,0);
+    hd44780_puts(&lcd, "rowing data:");
+
     printf("Minimum free heap size: %"PRIu32" bytes\n", esp_get_minimum_free_heap_size());
 
-    int interval = 0;
+    static char line2[16];
+    static int interval = 0;
     int last_time = esp_timer_get_time();
+    static int scaled_adc = 0;
     while (1) {
-        if (esp_timer_get_time() - last_time > 1000000) { // once / second
-            printf("interval: %d, triggers: %"PRIu32"\n", interval++, trigger_count);
-            trigger_count = 0;
+        if (esp_timer_get_time() - last_time > 500000) { // every half-second
             last_time = esp_timer_get_time();
+            ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_2, &adc_raw[0][0]));
+            scaled_adc = adc_raw[0][0]>>3;
+            printf("ADC: %d, triggers: %"PRIu32", interval %d\n", scaled_adc, trigger_count, interval++);
+            hd44780_gotoxy(&lcd, 0, 1);
+            snprintf(line2, 7, "%6d", scaled_adc);
+            hd44780_puts(&lcd, line2);
+            hd44780_gotoxy(&lcd, 8, 1);
+            snprintf(line2, 14, "%" PRIu32, trigger_count);
+            hd44780_puts(&lcd, line2);
+            trigger_count = 0;
         }
 
-        gpio_set_level(GPIO_OUTPUT_IO_0, 0);
-        gpio_set_level(GPIO_OUTPUT_IO_0, 1);
-        vTaskDelay(5);
+        // allow other tasks time to run
+        vTaskDelay(10);
     }
 }
